ebac6c5d4625f1544addf14df107b901c2323780
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-10 04:34:34 -0500

    Required the dependency for express, the router, and imported the model burger.js file to use its database functions.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+const burger = require('../models/burger.js');
+

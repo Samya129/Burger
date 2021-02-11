3d97d661386e9968ab3086ebbb9b52fa124df34f
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-10 23:40:27 -0500

    Fixed minor issue in controller.js file.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
   });
 
   router.post('/api/burgers', (req, res) => {
-    burger.create(['name', 'sleepy'], [req.body.burger_name, req.body.devoured], (result) => {
+    burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });

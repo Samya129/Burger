75ce17998f0666ca7f3039a558c80afcab672cf2
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-10 20:13:13 -0500

    Created and added a schema database and seeds file for the burger application.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burger_db;
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (1, "California Avocado Burger", true), (3, "Chilli Burger", true), (5, "Salmon Burger", true), (7, "Hamburger", true), (8, "Veggie Burger", true),
+(2, "Cheese Burger", false), (4, "Oklahoma Onion Burger", false), (6, "Steak Burger", false);

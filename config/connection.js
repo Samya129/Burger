8d7d58ff2fd34331f419c8ecb8d923a71e9e7df1
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-10 20:56:43 -0500

    Server WORKS! by adding the module.exports of router to the burgers_controller.js file.

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+// Set up MySQL connection:
+const mysql = require('mysql');
+
+require("dotenv").config();
+console.log(process.env.SECRETPASSWORD)
+
+//Connect the burger_db database using a localhost connection:
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.SECRETPASSWORD,
+  database: 'burger_db',
+});
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
+
+
+
+
+//NEED TO VERIFY THAT CONNECTION WORKS!!!!
